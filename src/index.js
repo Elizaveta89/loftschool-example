efdea677befe6a5d2848d2dbe06199332c2a73d5
Elizaveta <eovinova89@gmail.com>
2017-07-06 01:46:04 +0300

    начат фильтр по друзьям

--- src/index.js
+++ src/index.js
@@ -30,15 +30,113 @@ function vkInit() {
   });
 }
 
+
+var template = `
+{{#each items}}
+    <div class="friend" data-id="{{id}}">
+        <img src="{{photo_200}}">
+        <div class="name">{{first_name}} {{last_name}}</div>
+        <button class="button">+</button>
+    </div>
+{{/each}}
+`;
+
+var templateFn = Handlebars.compile(template);
+var leftListView = document.querySelector('.content-all');
+var rightListView = document.querySelector('.content-selected');
+var searchInputRight = document.querySelector('.search__input--right');
+var searchInputLeft = document.querySelector('.search__input--left');
+var wasInRight = false;
+var selectedElem;
+var selectedElemDeltaX = 0;
+var selectedElemDeltaY = 0;
+var leftListData = [];
+var rightListData = [];
+
+searchInputRight.addEventListener('input', function (e) {
+  var value = this.value;
+
+  rightListData = rightListData.filter(function(user) {
+    return user.indexOf(value) !== -1;
+  });
+
+  updateView();
+
+})
+
+searchInputLeft.addEventListener('input', function (e) {
+  var value = this.value;
+
+
+})
+
+window.addEventListener('mousemove', function (e) {
+  if (!selectedElem) return;
+
+  e.preventDefault();
+  selectedElem.style.left = e.pageX - selectedElemDeltaX + 'px';
+  selectedElem.style.top = e.pageY - selectedElemDeltaY + 'px';
+})
+
+window.addEventListener('mouseup', function(e) {
+  if (!selectedElem) return;
+
+  var selectedCenterX = e.pageX;
+  var body = document.querySelector('body');
+  var isInLeftRect = selectedCenterX < body.offsetWidth /2;
+  var isInRightRect = selectedCenterX > body.offsetWidth /2;
+  var idSelected = selectedElem.getAttribute('data-id');
+  var userData = getDataByUserId(idSelected);
+
+  if (isInLeftRect) {
+    leftListData.push(userData);
+  } else if (isInRightRect) {
+    rightListData.push(userData);
+  }
+  updateView();
+  selectedElem = null;
+})
+
+new Promise(resolve => window.onload = resolve)
+  .then(() => vkInit())
+  .then(() => vkApi('friends.get', {fields: 'photo_200'}))
+  .then(function(response) {
+    leftListData = response.items;
+    updateView();
+  })
+  // .catch(e => alert('Ошибка: ' + e.message));
+
 function updateView() {
   leftListView.innerHTML = templateFn({items: leftListData});
   rightListView.innerHTML = templateFn({items: rightListData});
+
+  var buttonPlus = document.querySelectorAll('.button');
+  for (var i=0; i <  buttonPlus.length; i++) {
+    buttonPlus[i].addEventListener('mousedown', function(e) {
+      e.stopPropagation();
+    })
+    buttonPlus[i].addEventListener('click', function(e) {
+      var thisButton = e.target;
+      var elemSelected = thisButton.parentNode;
+      var idSelected = elemSelected.getAttribute('data-id');
+      var userData = getDataByUserId(idSelected);
+
+      if (!wasInRight) {
+        rightListData.push(userData);
+      } else {
+        leftListData.push(userData);
+      }
+
+      updateView();
+      selectedElem = null;
+    });
+  }
 
   var friendElem = document.getElementsByClassName('friend');
   for(var i=0; i<friendElem.length; i++){
     friendElem[i].addEventListener('mousedown', function(e) {
       var elem = e.currentTarget;
-      elem.style.width = '300px';
+      elem.style.width = '400px';
       elem.style.position = 'absolute';
       elem.style.zIndex = '100';
 
@@ -59,6 +157,7 @@ function getDataByUserId (id) {
   }
 
   if (resultIndex != -1) {
+    wasInRight = false;
     return leftListData.splice(resultIndex, 1)[0];
   }
 
@@ -72,62 +171,7 @@ function getDataByUserId (id) {
   }
 
   if (resultIndex != -1) {
+    wasInRight = true;
     return rightListData.splice(resultIndex, 1)[0];
   }
 }
-
-var template = `
-{{#each items}}
-    <div class="friend" data-id="{{id}}">
-        <img src="{{photo_200}}">
-        <div class="name">{{first_name}} {{last_name}}</div>
-        <button class="button">+</button>
-    </div>
-{{/each}}
-`;
-
-var templateFn = Handlebars.compile(template);
-var leftListView = document.querySelector('.content-all');
-var rightListView = document.querySelector('.content-selected');
-var selectedElem;
-var selectedElemDeltaX = 0;
-var selectedElemDeltaY = 0;
-var leftListData = [];
-var rightListData = [];
-
-window.addEventListener('mousemove', function (e) {
-  if (!selectedElem) return;
-
-  e.preventDefault();
-  selectedElem.style.left = e.pageX - selectedElemDeltaX + 'px';
-  selectedElem.style.top = e.pageY - selectedElemDeltaY + 'px';
-})
-
-window.addEventListener('mouseup', function(e) {
-  if (!selectedElem) return;
-
-  var selectedCenterX = e.pageX;
-  var selectedCenterY = e.pageY;
-  var body = document.querySelector('body');
-  var isInLeftRect = selectedCenterX < body.offsetWidth /2;
-  var isInRightRect = selectedCenterX > body.offsetWidth /2;
-  var idSelected = selectedElem.getAttribute('data-id');
-  var userData = getDataByUserId(idSelected);
-
-  if (isInLeftRect) {
-    leftListData.push(userData);
-  } else if (isInRightRect) {
-    rightListData.push(userData);
-  }
-  updateView();
-  selectedElem = null;
-})
-
-new Promise(resolve => window.onload = resolve)
-  .then(() => vkInit())
-  .then(() => vkApi('friends.get', {fields: 'photo_200'}))
-  .then(function(response) {
-    leftListData = response.items.splice(0, 5);
-    updateView();
-  })
-  // .catch(e => alert('Ошибка: ' + e.message));
